9caac5a347799b14ecb2db65dadc46107821f002
Aryan Kothariset -g allow-passthrough on <87589047+thearyadev@users.noreply.github.com>
2024-11-21 16:22:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# tubearchivist pruner
+the builtin pruner wasnt working. 
+
+this script will delete all videos that are watched and older than a specified number of days 
+
+## Usage
+### cli 
+
+can be run with python 3.12
+
+```bash
+> python main.py
+
+usage: tubearchivist-pruner [-h] -a MIN_WATCHED_AGE -u URL -t TOKEN [-e]
+                            [-s SLEEP]
+
+this script will delete all videos that are watched and older than a specified
+number of days
+
+options:
+  -h, --help            show this help message and exit
+  -a MIN_WATCHED_AGE, --min-watched-age MIN_WATCHED_AGE
+                        Min age in days from the watched date
+  -u URL, --url URL     Tube archivist API url
+  -t TOKEN, --token TOKEN
+                        Tube archviist api token
+  -e, --endless
+  -s SLEEP, --sleep SLEEP
+```
+
+### docker (compose)
+```yaml
+services:
+    ta-pruner:
+        image: thearyadev0/tubearchivist-pruner:latest # or version tag
+        environment:
+            TUBEARCHIVIST_URL: ""
+            API_TOKEN: ""
+            PRUNE_OLDER_THAN: "" # number of days. All videos older than this, that are watched, will be deleted.
+            SLEEP: "" # OPTONAL: number of seconds to wait before running again
+```
+
+
---
+++ main.py
@@ -0,0 +1,108 @@
+import argparse
+import logging
+import typing
+import datetime
+import time
+
+import requests
+
+LIST_VIDEOS_PATH = "/api/video/"
+DELETE_VIDEO_PATH = "/api/video/%s/"
+type Video = dict[str, typing.Any]
+type VideoArray = list[Video]
+
+
+def fetch_all_videos(api_url: str, api_key: str) -> VideoArray:
+    headers = {"Authorization": f"Token {api_key}"}
+    videos: list[dict] = list()
+    page = 1
+    last_page_reached = True
+    while last_page_reached:
+        logging.info(f"Fetching {page=} from tube archivist")
+        request = requests.get(
+            url=f"{api_url}{LIST_VIDEOS_PATH}?page={page}", headers=headers
+        )
+        if request.status_code != 200:
+            logging.error("Recieved non-200 status code from Tube archivist when fetching video lists")
+            logging.error(request.content)
+            raise Exception("Failed to reach tubearchivist")
+        if len(content := request.json()["data"]) != 0:
+            videos.extend(content)
+            page += 1
+            continue
+        last_page_reached = False
+    return videos
+
+def delete_video(video: Video, api_url: str, api_key: str) -> None:
+    headers = {"Authorization": f"Token {api_key}"}
+    url = f"{api_url}{DELETE_VIDEO_PATH % video.get('youtube_id')}"
+    logging.info(f"Deleting: {url.replace('/api', '')}")
+    request = requests.delete(url, headers=headers)
+    if request.status_code != 200:
+        logging.error("Recieved non-200 status code from Tube archivist when deleting")
+    return None
+
+def filter_watched(videos: VideoArray) -> VideoArray:
+    return [v for v in videos if v["player"]["watched"] == True]
+
+
+def filter_watched_date(videos: VideoArray, cutoff: datetime.datetime) -> VideoArray:
+    return [
+        v
+        for v in videos
+        if datetime.datetime.fromtimestamp(v["date_downloaded"]) < cutoff
+    ]
+
+
+def prune(
+    api_url: str,
+    min_age: int,
+    api_key: str,
+) -> None:
+    videos = fetch_all_videos(api_url, api_key)
+    logging.info(f"Found {len(videos)} videos. Filtering age and watch status...")
+    videos = filter_watched(videos)
+    videos = filter_watched_date(
+        videos, cutoff=datetime.datetime.now() - datetime.timedelta(days=min_age)
+    )
+    logging.info(f"Filtered results to {len(videos)} videos.")
+    for v in videos:
+        delete_video(v, api_url, api_key)
+
+def main() -> int:
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(
+        prog="tubearchivist-pruner", description="this script will delete all videos that are watched and older than a specified number of days"
+    )
+
+    parser.add_argument(
+        "-a",
+        "--min-watched-age",
+        required=True,
+        type=int,
+        help="Min age in days from the watched date",
+    )
+    parser.add_argument(
+        "-u", "--url", required=True, type=str, help="Tube archivist API url"
+    )
+    parser.add_argument("-t", "--token", type=str, required=True, help="Tube archviist api token")
+    parser.add_argument("-e", "--endless", action="store_true")
+    parser.add_argument("-s", "--sleep", type=int, required=False, default=10)
+    args = parser.parse_args()
+
+    while True:
+        prune(
+            api_url=args.url,
+            min_age=args.min_watched_age,
+            api_key=args.token,
+        )
+        
+        if not args.endless:
+            return 0
+
+        logging.info(f"running endless... sleeping for {args.sleep} seconds")
+        time.sleep(args.sleep)
+        
+
+if __name__ == "__main__":
+    raise SystemExit(main())
